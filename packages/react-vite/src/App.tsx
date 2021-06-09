e295d362587de37919412f53b07407e349580da1
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-09 09:28:00 -0300

    added vite sample

---
+++ packages/react-vite/src/App.tsx
@@ -0,0 +1,13 @@
+import { SimpleText } from '@pauloluan/shared-components'
+import React from 'react'
+
+function App () {
+  return (
+    <>
+      Isso usa Styled:
+      <SimpleText />
+    </>
+  )
+}
+
+export default App
