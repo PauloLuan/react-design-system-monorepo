38ffe4e4a74a5a1651c7dcb76a2b07399e9f9337
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-09 09:41:11 -0300

    🎉 fix: added nextjs consuming from shared

---
+++ packages/next-js-app/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@chakra-ui/layout'
+import { SimpleText } from '@pauloluan/shared-components'
+
+const Index = () => (
+  <Container>
+    MINDINGOS
+    <SimpleText />
+  </Container>
+)
+
+export default Index
